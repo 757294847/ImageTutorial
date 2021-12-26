dcf4ea4cbfe282ea02bcb3c8019e76aef38bfb45
Admin <Admin@DESKTOP-DUESHBO>
2021-12-27 02:24:31 +0300

    Добавьте файлы проекта.

---
+++ ImageTutorial/ImageTutorial/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using ImageTutorial.Services;
+using ImageTutorial.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ImageTutorial
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+            Console.WriteLine("OnStart");
+        }
+
+        protected override void OnSleep()
+        {
+            Console.WriteLine("OnSleep");
+        }
+
+        protected override void OnResume()
+        {
+            Console.WriteLine("OnResume");
+        }
+    }
+}
---
+++ ImageTutorial/ImageTutorial/Views/AboutPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ImageTutorial.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+
+        }
+
+        async void OnDisplayAlertButtonClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Alert", "This is an alert.", "OK");
+        }
+
+        async void OnDisplayAlertQuestionButtonClicked(object sender, EventArgs e)
+        {
+            bool response = await DisplayAlert("Save?", "Would you like to save your data?", "Yes", "No");
+            Console.WriteLine("Save data: " + response);
+        }
+    }
+}
